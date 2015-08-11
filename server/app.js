3439742508537e9b41528a3c608163075db18c8e
Brian Leathem <bleathem@gmail.com>
2015-08-11 16:24:22 -0600

    Renamed "static" to "portal"

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.set('port', config.get('PORT'));
 app.set('base url', config.get('IP'));
 app.use(middle.cors);
 
-app.use(express.static(__dirname + '/../static'));
+app.use(express.static(__dirname + '/../portal'));
 app.use('/node_modules', express.static(__dirname + '/../node_modules'));
 app.use('/api', router);
 app.use(middle.logError);
