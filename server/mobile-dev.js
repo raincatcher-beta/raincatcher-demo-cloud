7752e34f898e97803cce5680315daab5e97b39b3
Brian Leathem <bleathem@gmail.com>
2015-08-13 10:10:10 -0600

    Moved the mobile server script into the server folder

---
+++ server/mobile-dev.js
@@ -0,0 +1,19 @@
+'use strict';
+/**
+* Serve the mobile environment on it's own port
+**/
+
+var http = require('http')
+  , express = require('express')
+  , app = express()
+  , port = 8081
+  , ip = '0.0.0.0'
+  ;
+
+app.set('port', port);
+app.set('base url', ip);
+app.use(express.static(__dirname + '/../www'));
+
+var server = http.createServer(app);
+server.listen(port, ip);
+console.log('Serving www/ on ' + ip + ':' + port);
