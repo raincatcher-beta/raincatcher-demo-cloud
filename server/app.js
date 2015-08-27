8a659b40ceb88239ad5eeea2d38714c33c3259e5
Brian Leathem <bleathem@gmail.com>
2015-08-27 14:10:12 -0700

    Added a server-side WFM implementation for serving workorders

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ var express = require('express')
   , app = express()
   , config = require('./config')
   , middle = require('./middleware')
-  , router = express.Router()
+  , mediator = require('wfm-mediator/mediator')
   ;
 
 app.set('port', config.get('PORT'));
@@ -13,8 +13,22 @@ app.use(middle.cors);
 
 app.use(express.static(__dirname + '/../portal'));
 app.use('/node_modules', express.static(__dirname + '/../node_modules'));
+
+// app specific router
+var router = express.Router();
+router.get('/hello', function (req, res, next) {
+  res.send('hello');
+});
 app.use('/api', router);
+
+// setup the wfm routes
+require('wfm-workorder/router')(mediator, app);
+
+// error handling
 app.use(middle.logError);
 app.use(middle.handleError);
+
+// app modules
+require('./app/workorder')(mediator);
 
 module.exports = exports = app;
---
+++ server/app/workorder.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var workorders = [
+  { id: 1276001, type: 'Job Order', title: 'Footpath in disrepair', status: 'In Progress', address: '118 N Peoria @N Chicago, IL 60607', summary: 'Please remove damaged kerb and SUPPLY AND FIX 1X DROP KERB CENTRE BN 125 X 150 cart away from site outside number 3.'},
+  { id: 1276231, type: 'Job Order', title: 'Road in disrepair', status: 'Complete', address: '2116 Sussex Dr. @Redmond, WA 60607', summary: 'Please remove damaged kerb and SUPPLY AND FIX 1X DROP KERB CENTRE BN 125 X 150 cart away from site outside number 3.'},
+  { id: 1276712, type: 'Job Order', title: 'Driveway in disrepair', status: 'Aborted', address: '18 Curve Cr. @San Jose, CA 60607', summary: 'Please remove damaged kerb and SUPPLY AND FIX 1X DROP KERB CENTRE BN 125 X 150 cart away from site outside number 3.'},
+  { id: 1262134, type: 'Job Order', title: 'Door in disrepair', status: 'On Hold', address: '623 Ferry St. @Boise, ID 60607', summary: 'Please remove damaged kerb and SUPPLY AND FIX 1X DROP KERB CENTRE BN 125 X 150 cart away from site outside number 3.'},
+  { id: 12623122, type: 'Job Order', title: 'Roof in disrepair', status: 'Unassigned', address: '5528 Closed loop @Boston, MA 60607', summary: 'Please remove damaged kerb and SUPPLY AND FIX 1X DROP KERB CENTRE BN 125 X 150 cart away from site outside number 3.'},
+  { id: 12623122, type: 'Job Order', title: 'House in disrepair', status: 'New', address: '364 Driver way @Portland, OR 60607', summary: 'Please remove damaged kerb and SUPPLY AND FIX 1X DROP KERB CENTRE BN 125 X 150 cart away from site outside number 3.'}
+];
+
+module.exports = function(mediator) {
+  console.log('Subscribing to mediator topic: workorders:load');
+  mediator.subscribe('workorders:load', this, function() {
+    setTimeout(function() {
+      mediator.publish('workorders:loaded', this, workorders);
+    }, 0);
+  });
+}
