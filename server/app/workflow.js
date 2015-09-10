76f7b8d819bd7a6771b5919038a55d1cd39fd900
Brian Leathem <bleathem@gmail.com>
2015-09-10 16:08:02 -0700

    Created a set of "portal views" for the existing WFM workflow modules

--- server/app/workflow.js
+++ server/app/workflow.js
@@ -3,15 +3,24 @@
 var steps = [
   {code: 'begin-workflow', name: 'Begin Workflow', templates: {
     form: '<workorder-list-item workorder="workorder"></workorder-view-item>',
-    view: '<workorder-list-item workorder="workorder"></workorder-view-item>'
+    view: '<workorder-list-item workorder="workorder"></workorder-view-item>',
+    portal: {
+      view: '<workorder-portal-view value="workorder"></workorder-portal-view>'
+    }
   }},
   {code: 'risk-assessment', name: 'Risk Assessment', templates: {
     form: '<risk-assessment-form></risk-assessment-form>',
-    view: '<risk-assessment value="workorder.riskAssessment"></risk-assessment>'
+    view: '<risk-assessment value="workorder.riskAssessment"></risk-assessment>',
+    portal: {
+      view: '<risk-assessment-portal-view value="workorder.riskAssessment"></risk-assessment-portal-view>'
+    }
   }},
   {code: 'vehicle-inspection', name: 'Vehicle Inspection', templates: {
     form: '<vehicle-inspection-form></vehicle-inspection-form>',
-    view: '<vehicle-inspection value="workorder.vehicleInspection"></vehicle-inspection>'
+    view: '<vehicle-inspection value="workorder.vehicleInspection"></vehicle-inspection>',
+    portal: {
+      view: '<vehicle-inspection-portal-view value="workorder.riskAssessment"></vehicle-inspection-portal-view>'
+    }
   }},
   {code: 'workflow-complete', name: 'Workflow Complete', templates: {
     form: '<workflow-step-summary workorder="workorder" steps="steps"></workflow-step-summary>',
