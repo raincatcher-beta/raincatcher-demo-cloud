bc7e81a44781ba69ad0c2460d7c77a101b0a8e94
Brian Leathem <bleathem@gmail.com>
2015-09-20 05:54:48 -0700

    Removed non-cloud npm run scripts, added FH dependencies

--- server/app.js
+++ server/app.js
@@ -1,38 +1,42 @@
 'use strict';
 
-var express = require('express')
+var mbaasApi = require('fh-mbaas-api')
+  , express = require('express')
   , app = express()
+  , mbaasExpress = mbaasApi.mbaasExpress()
   , config = require('./config')
   , middle = require('./middleware')
   , mediator = require('wfm-mediator/mediator')
   , bodyParser = require('body-parser')
   ;
 
+var securableEndpoints = [];
+
 app.set('port', config.get('PORT'));
 app.set('base url', config.get('IP'));
 app.use(middle.cors);
-app.use(bodyParser.json({limit: '10mb'}));
 
-app.use(express.static(__dirname + '/../portal'));
-app.use('/node_modules', express.static(__dirname + '/../node_modules'));
+app.use('/sys', mbaasExpress.sys(securableEndpoints));
+app.use('/mbaas', mbaasExpress.mbaas);
+
+app.use(mbaasExpress.fhmiddleware());
+
+// app.use(bodyParser.json({limit: '10mb'}));
 
 // app specific router
 var router = express.Router();
-router.get('/hello', function (req, res, next) {
-  res.send('hello');
-});
 app.use('/api', router);
 
 // setup the wfm routes
 require('wfm-workorder/router')(mediator, app);
 require('wfm-workflow/router')(mediator, app);
 
-// error handling
-app.use(middle.logError);
-app.use(middle.handleError);
-
 // app modules
 require('./app/workorder')(mediator);
 require('./app/workflow')(mediator);
+
+// error handling
+app.use(mbaasExpress.errorHandler());
+
 
 module.exports = exports = app;
