5cf6efdc1f76d5685c87608df8978324988698fd
Brian Leathem <bleathem@gmail.com>
2015-11-09 19:03:34 -0800

    Added an example consuming angular service

---
+++ test/client/sample-service.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = 'sync-service.consumer';
+
+angular.module('sync-service.consumer', [
+, require('../../lib/wrappers/sync-angular')
+])
+.factory('object1Sync', function(syncService) {
+  syncService.init(window.$fh, {do_console_log: false});
+  var object1Sync = {};
+  object1Sync.managerPromise = syncService.manage('sample-dataset-id-1')
+  .then(function(_manager) {
+    object1Sync.manager = _manager;
+  });
+
+  return object1Sync;
+})
+.factory('object2Sync', function(syncService) {
+  syncService.init(window.$fh, {do_console_log: false});
+  var object2Sync = {};
+  object2Sync.managerPromise = syncService.manage('sample-dataset-id-2')
+  .then(function(_manager) {
+    object2Sync.manager = _manager;
+  });
+
+  return object2Sync;
+})
---
+++ test/client/sample-service.spec.js
@@ -0,0 +1,88 @@
+'use strict'
+
+debugger;
+var $fh = require('../lib/feedhenry')
+  , should = require('should')
+  , config = require('../test-config')
+  , mediator = require('fh-wfm-mediator/mediator')
+  , q = require('q')
+  , helper = require('./test-helper')
+  , testData = require('../test-data')
+  , angular = require('angular')
+  ;
+
+require('angular-mocks');
+// alternative to loading fhconfig via xhr
+window.fh_app_props = require('../lib/fhconfig.json');
+window.$fh = $fh;
+window.angular = angular;
+
+require('./sample-service')
+
+describe('Test a consumer of the sync framework', function() {
+  var object1Sync, object2Sync, subscription;
+
+  before(function() {
+    localStorage.clear();
+    helper.overrideNavigator();
+    return q.all([
+      helper.syncServerInit($fh, 'sample-dataset-id-1'),
+      helper.syncServerInit($fh, 'sample-dataset-id-2')
+    ]);
+  });
+
+  after(function() {
+    return q.all([
+      helper.syncServerStop($fh, 'sample-dataset-id-1'),
+      helper.syncServerStop($fh, 'sample-dataset-id-2')
+    ])
+    .then(function() {
+      helper.restoreNavigator();
+    });
+  });
+
+  beforeEach('angular init', function() {
+    angular.mock.module('sync-service.consumer');
+    angular.mock.module(function($provide) {
+      $provide.value('$q', q);
+    });
+    angular.mock.inject(function (_object1Sync_, _object2Sync_) {
+      object1Sync = _object1Sync_;
+      object2Sync = _object2Sync_;
+    });
+    return object1Sync.managerPromise
+  });
+
+  beforeEach('wait for sync', function() {
+    return q.all([
+      object1Sync.manager.waitForSync(),
+      object2Sync.manager.waitForSync()
+    ])
+  });
+
+  afterEach(function() {
+    return q.all([
+      object1Sync.manager.stop(),
+      object2Sync.manager.stop()
+    ])
+  });
+
+  it('nothing blows up', function() {
+    'true'.should.be.equal('true');
+  });
+
+  it('list 1 result is correct', function() {
+    return object1Sync.manager.list()
+    .then(function(result) {
+      result.should.have.length(testData.length);
+    });
+  });
+
+  it('list 2 result is correct', function() {
+    return object2Sync.manager.list()
+    .then(function(result) {
+      result.should.have.length(testData.length);
+    });
+  });
+
+});
