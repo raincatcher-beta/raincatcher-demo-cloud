be48b60bf97f31201cbba7fc25df7352331357ab
Brian Leathem <bleathem@gmail.com>
2016-02-16 19:11:43 -0800

    Added copyright headers to all the files

--- test/test-data.js
+++ test/test-data.js
@@ -1,3 +1,8 @@
+/**
+* CONFIDENTIAL
+* Copyright 2016 Red Hat, Inc. and/or its affiliates.
+* This is unpublished proprietary source code of Red Hat.
+**/
 module.exports = [
   { id: 1276001, user: 'john', workflowId: '0', type: 'Job Order', title: 'Footpath in disrepair', status: 'In Progress', finishTimestamp: '2015-10-22T07:00:00Z', address: '118 N Peoria @N Chicago, IL 60607', summary: 'Please remove damaged kerb and SUPPLY AND FIX 1X DROP KERB CENTRE BN 125 X 150 cart away from site outside number 3.'},
   { id: 1276231, user: 'john', workflowId: '1', type: 'Job Order', title: 'Road in disrepair', status: 'Complete', finishTimestamp: '2015-10-22T07:00:00Z', address: '2116 Sussex Dr. @Redmond, WA 60607', summary: 'Please remove damaged kerb and SUPPLY AND FIX 1X DROP KERB CENTRE BN 125 X 150 cart away from site outside number 3.'},
