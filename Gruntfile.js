10ddb030a541b91cd89671152947b9838b953017
Daniel Bevenius <daniel.bevenius@gmail.com>
2016-10-10 09:23:12 +0200

    update eslint test directory regex
    Also including a test/.eslintrc configuration file that is specific for
    tests.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
   'use strict';
   grunt.initConfig({
     eslint: {
-      src: ["lib/**/*.js", "test/**/*.js"]
+      src: ["lib/**/*.js", "test/**/*.spec.js"]
     }
   });
   grunt.loadNpmTasks("grunt-eslint");
