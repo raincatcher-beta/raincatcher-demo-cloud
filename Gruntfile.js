fabec53d6dbcf5aba6d05a7b80d66990eebd1b98
Niall Donnelly <niall.donnelly@feedhenry.com>
2017-01-26 17:24:21 +0000

    Refactored the raincatcher-sync module and included the subscriber to sync notifications

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,21 @@
 module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
   'use strict';
   grunt.initConfig({
     eslint: {
       src: ["lib/**/*.js", "test/**/*.spec.js"]
+    },
+    mochify: {
+      options: {
+        reporter: 'spec'
+      },
+      unit: {
+        src: ['lib/**/**/*-spec.js']
+      }
     }
   });
-  grunt.loadNpmTasks("grunt-eslint");
+
+  grunt.registerTask('test', ['eslint', 'mochify:unit']);
   grunt.registerTask('default', ['eslint']);
 };
